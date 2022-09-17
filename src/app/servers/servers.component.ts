d247651076bfee6dea6a01330cc5db7389368d28
vickythakur0398 <vickythakur0398@gmail.com>
2022-09-17 18:50:26 +0530

    assignmnet last of first module

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,9 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'Server Not Created';
   serverName = '';
+  nameCreated = false;
+  servers = ['TestServer', 'TestServer2'];
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -18,6 +21,8 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.nameCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was Created! Name is' + this.serverName;
   }
 
@@ -31,5 +36,17 @@ export class ServersComponent implements OnInit {
   allowb = false;
   onEmpty() {
     this.userName = '';
+  }
+
+  //assiggnmnert 3
+  dis = false;
+  cLog = [];
+  onDisplay() {
+    this.cLog.push(this.cLog.length + 1);
+    if (this.dis == false) {
+      this.dis = true;
+    } else {
+      this.dis = false;
+    }
   }
 }
